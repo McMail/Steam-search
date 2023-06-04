9c21876deb8fdc210b6fbf51674193b6f30ccfb5
McMail <truemcmail@gmail.com>
2023-06-04 14:20:07 +1000

    made

--- SteamAPIuser.js
+++ SteamAPIuser.js
@@ -3,56 +3,37 @@ const steamApiKey = "7479F75666B66D5E5D1DB8525C3938B6";
 
 // DOM elements
 const steamLoginBtn = document.getElementById("steam-login-btn");
-const profileContainer = document.getElementById("profile-container");
 
-// Steam authentication URL
-const steamAuthUrl = `https://steamcommunity.com/openid/login?` +
-  `openid.ns=http://specs.openid.net/auth/2.0&` +
-  `openid.mode=checkid_setup&` +
-  `openid.return_to=${encodeURIComponent(window.location.href + "verify")}&` +
-  `openid.realm=${encodeURIComponent(window.location.href)}`;
-
-// Sign in with Steam button click event handler
+// Steam authentication handler
 steamLoginBtn.addEventListener("click", () => {
-  window.location.href = steamAuthUrl;
+  // Redirect the user to the Steam login page
+  SteamOpenID.login(steamApiKey, window.location.href + "verify");
 });
 
-// Function to get URL parameter value
-function getUrlParameter(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  const regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
-  const results = regex.exec(window.location.search);
-  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+// Function to handle the authentication response
+function verifyCallback(response) {
+  if (response.status === "success") {
+    // Authentication successful, retrieve user information
+    const steamId = response.steamid;
+    const steamProfileUrl = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamApiKey}&steamids=${steamId}`;
 
-// Check if the page has the authentication response
-const openIdParams = getUrlParameter("openid.identity");
-if (openIdParams) {
-  // Verify authentication response on the server-side using your backend
-  // Make a request to your backend server with the authentication response to validate it
-  // Retrieve the user's Steam ID from the response and fetch the Steam profile data using the Steam Web API
+    fetch(steamProfileUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        // Extract the profile data from the response
+        const profileData = data.response.players[0];
 
-  // Example implementation
-  const steamId = openIdParams.replace("https://steamcommunity.com/openid/id/", "");
-  const steamProfileUrl = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamApiKey}&steamids=${steamId}`;
-
-  fetch(steamProfileUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      // Extract the profile data from the response
-      const profileData = data.response.players[0];
-
-      // Display the Steam profile information
-      const profileHtml = `
-        <img src="${profileData.avatarfull}" alt="Profile Picture">
-        <p>Steam ID: ${profileData.steamid}</p>
-        <p>Display Name: ${profileData.personaname}</p>
-        <p>Profile URL: <a href="${profileData.profileurl}" target="_blank">${profileData.profileurl}</a></p>
-      `;
-      profileContainer.innerHTML = profileHtml;
-    })
-    .catch((error) => {
-      console.error(error);
-      profileContainer.textContent = "Failed to fetch Steam profile.";
-    });
+        // Display the Steam profile information
+        console.log("Steam ID:", profileData.steamid);
+        console.log("Display Name:", profileData.personaname);
+        console.log("Profile URL:", profileData.profileurl);
+      })
+      .catch((error) => {
+        console.error(error);
+        console.log("Failed to fetch Steam profile.");
+      });
+  } else {
+    // Authentication failed or cancelled by the user
+    console.log("Steam authentication failed.");
+  }
 }
